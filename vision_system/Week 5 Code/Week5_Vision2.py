8758ba38afe90b0cdff3cd05cf049695d3970092
KOTA-01 <j.bird02@outlook.com>
2023-09-14 11:29:47 +1000

    updates

--- vision_system/Week 5 Code/Week5_Vision2.py
+++ vision_system/Week 5 Code/Week5_Vision2.py
@@ -1,33 +1,31 @@
 import cv2
 import numpy as np
-import time
-
-# Simulated data: Distance (in cm) and corresponding pixel counts
-distances = [15, 25, 30, 35, 40, 45, 50]
-pixel_counts = [125000, 53000, 40000, 26000, 21000, 16000, 13000]
+import time  # Import the time module
 
 # Initialize camera
 cap = cv2.VideoCapture(0)
 
 # Set camera properties
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 308)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 410)
 
 # Initialize windows
 cv2.namedWindow('Camera Feed')
 cv2.namedWindow('Masked View')
 
-# Define lower and upper bounds for orange color in HSV
-lower_bound = np.array([10, 100, 100])
-upper_bound = np.array([30, 255, 255])
+# Define lower and upper bounds for Black color in HSV
+lower_bound = np.array([0, 0, 0])
+upper_bound = np.array([179, 255, 50])
 
-# Fit a polynomial regression model for pixel-to-distance mapping
-coefficients = np.polyfit(pixel_counts, distances, 2)
-pixel_to_distance = np.poly1d(coefficients)
+# Camera calibration parameters (adjust as needed)
+focal_length = 500  # Focal length in pixels
+known_width = 10.0  # Known width of a black blob in centimeters
+
+# Measure execution time: Start the timer
+start_time = time.time()
 
 # Main loop for processing video frames
 while True:
-    start_time = time.time()
 
     # Capture a frame from the camera
     ret, frame = cap.read()
@@ -37,63 +35,68 @@ while True:
     # Convert the frame to HSV color space
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Create a mask for the orange color
-    orange_mask = cv2.inRange(hsv_frame, lower_bound, upper_bound)
+    # Create a mask for the black color
+    black_mask = cv2.inRange(hsv_frame, lower_bound, upper_bound)
 
     # Find contours in the mask
-    contours, _ = cv2.findContours(orange_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Find the contour with the largest area (majority of orange pixels)
-    largest_contour = max(contours, key=cv2.contourArea, default=None)
-
-    # Find the bounding rectangle of the largest contour
-    x, y, w, h = cv2.boundingRect(largest_contour)
-
-    # Check if the bounding rectangle touches the edge of the frame
-    edge_touching = x == 0 or y == 0 or x + w == frame.shape[1] or y + h == frame.shape[0]
-
-    if edge_touching:
-        # Draw a red rectangle around the largest contour
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        cv2.putText(frame, "Edge Touching", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-    # Calculate the center of the largest contour
-    M = cv2.moments(largest_contour)
-    if M["m00"] != 0:
-        cx = int(M["m10"] / M["m00"])
-        cy = int(M["m01"] / M["m00"])
-        
-        # Draw a blue circle at the center
-        cv2.circle(frame, (cx, cy), 5, (255, 0, 0), -1)
-        cv2.putText(frame, f'({cx},{cy})', (cx + 10, cy - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-
-        # Calculate the number of orange pixels in the largest contour
-        num_orange_pixels = cv2.contourArea(largest_contour)
-
-        # Estimate the distance using pixel-to-distance mapping
-        estimated_distance = pixel_to_distance(num_orange_pixels)
-
-        # Display the estimated distance on the frame
-        cv2.putText(frame, f'Estimated Distance: {estimated_distance:.2f} cm', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    contours, _ = cv2.findContours(black_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Create a masked view using bitwise operation
-    masked_view = cv2.bitwise_and(frame, frame, mask=orange_mask)
+    masked_view = cv2.bitwise_and(frame, frame, mask=black_mask)
 
-    # Draw the contour on the original frame
-    if largest_contour is not None:
-        cv2.drawContours(frame, [largest_contour], -1, (0, 255, 0), 3)
+    # Initialize a count for substantial black blobs
+    blob_count = 0
+
+    # Iterate through the detected contours
+    for contour in contours:
+        # Calculate the area of the contour
+        area = cv2.contourArea(contour)
+
+        # Define a threshold for considering a contour as a substantial blob
+        min_blob_area = 1000  # Adjust this threshold as needed
+
+        if area >= min_blob_area:
+            # Find the bounding rectangle of the current contour
+            x, y, w, h = cv2.boundingRect(contour)
+
+            # Check if the bounding rectangle touches the edge of the frame
+            edge_touching = x == 0 or y == 0 or x + w == frame.shape[1] or y + h == frame.shape[0]
+
+            # Draw the contour (outline) around the blob
+            cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
+
+            # If the contour touches the edge, mark it as an error
+            if edge_touching:
+                cv2.putText(frame, "Edge Touching", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            else:
+                # Increment the blob count
+                blob_count += 1
+
+            # Calculate the distance to the blob using the known width and focal length
+            distance = (known_width * focal_length) / cv2.arcLength(contour, True)  # Distance in centimeters
+
+            # Display the distance on the frame
+            cv2.putText(frame, f'Distance: {distance:.2f} cm', (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     # Display the camera feed and the masked view
     cv2.imshow('Camera Feed', frame)
     cv2.imshow('Masked View', masked_view)
+
+    # Display the count of detected substantial black blobs
+    cv2.putText(frame, f'Aisle Count: {blob_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow('Camera Feed', frame)
 
     # Wait for a key press and check if it's the 'q' key to exit the loop
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
 
-    end_time = time.time()
-    #print(f"Frame processing time: {end_time - start_time:.4f} seconds")
+# Measure execution time: Stop the timer
+end_time = time.time()
+
+# Calculate and display the total execution time
+execution_time = end_time - start_time
+print(f"Total Execution Time: {execution_time:.2f} seconds")
 
 # Release the camera and close all windows
 cap.release()
--- vision_system/Week 8 code/BlackDots.py
+++ vision_system/Week 8 code/BlackDots.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time  # Import the time module
 
 # Initialize camera
 cap = cv2.VideoCapture(0)
@@ -19,6 +20,9 @@ upper_bound = np.array([179, 255, 50])
 # Camera calibration parameters (adjust as needed)
 focal_length = 500  # Focal length in pixels
 known_width = 10.0  # Known width of a black blob in centimeters
+
+# Measure execution time: Start the timer
+start_time = time.time()
 
 # Main loop for processing video frames
 while True:
@@ -52,30 +56,47 @@ while True:
         min_blob_area = 1000  # Adjust this threshold as needed
 
         if area >= min_blob_area:
+            # Find the bounding rectangle of the current contour
+            x, y, w, h = cv2.boundingRect(contour)
+
+            # Check if the bounding rectangle touches the edge of the frame
+            edge_touching = x == 0 or y == 0 or x + w == frame.shape[1] or y + h == frame.shape[0]
+
             # Draw the contour (outline) around the blob
             cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
 
-            # Increment the blob count
-            blob_count += 1
+            # If the contour touches the edge, mark it as an error
+            if edge_touching:
+                cv2.putText(frame, "Edge Touching", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            else:
+                # Increment the blob count
+                blob_count += 1
 
             # Calculate the distance to the blob using the known width and focal length
             distance = (known_width * focal_length) / cv2.arcLength(contour, True)  # Distance in centimeters
 
             # Display the distance on the frame
-            cv2.putText(frame, f'Distance: {distance:.2f} cm', (contour[0][0][0], contour[0][0][1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.putText(frame, f'Distance: {distance:.2f} cm', (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     # Display the camera feed and the masked view
     cv2.imshow('Camera Feed', frame)
     cv2.imshow('Masked View', masked_view)
 
     # Display the count of detected substantial black blobs
-    cv2.putText(frame, f'Aisle: {blob_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame, f'Aisle Count: {blob_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.imshow('Camera Feed', frame)
 
     # Wait for a key press and check if it's the 'q' key to exit the loop
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
+
+# Measure execution time: Stop the timer
+end_time = time.time()
+
+# Calculate and display the total execution time
+execution_time = end_time - start_time
+print(f"Total Execution Time: {execution_time:.2f} seconds")
 
 # Release the camera and close all windows
 cap.release()
