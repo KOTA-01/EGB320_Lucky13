616c4cc142e30f87ebf61d7703b41d5133968622
OniQUT <145178398+OniQUT@users.noreply.github.com>
2023-10-08 21:04:20 +1000

    updated nav

--- navigation/state_machine.py
+++ navigation/state_machine.py
@@ -13,27 +13,44 @@ if __name__ == '__main__':
 		order_reader = OrderReader()
 		bot = robot()
 		# State machine initialization
-		order = 1
-		aisleNav = 2
-		pick_item = 3
+		order = 2
+		drive = 3
+		aisleNav = 4
+		exit_aisle = 5
+		drop_off = 6
+		done = 7
 
 		state = order
 		
 		while True:
-
 			if state == order:
 				order_data = order_reader.ReadOrder("Order_1.csv")
 				print(order_data)
+				print("RED LED - I am searching for the item!")
+				state = drive
+			
+			elif state == drive:
+				bot.nav_to_aisle()
 				state = aisleNav
 
 			elif state == aisleNav:
 				bot.run()
-				
-				state = pick_item
+				state = exit_aisle
 
-			elif state == pick_item:
-				motor.SetTargetVelocities(0, 0)
-				"""Code for picking the item/calling to a function that does that"""
+			elif state == exit_aisle:
+				print("Exiting aisle")
+				bot.exiting()
+				state = drop_off
+
+			elif state == drop_off:
+				print("Searching for Packing bay")
+				bot.navigate_to_drop_off()
+				state = done
+
+			elif state == done:
+				print("Completed :3")
+				self.SetTargetVelocities(0, 0)
+				break
 
 	except KeyboardInterrupt as e:
 		motor.SetTargetVelocities(0, 0)
