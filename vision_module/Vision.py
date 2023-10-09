26f0274c50c39f999f06a1527abb71d648c37e52
KOTA-01 <j.bird02@outlook.com>
2023-10-10 00:09:29 +1000

    updates

--- vision_module/Vision.py
+++ vision_module/Vision.py
@@ -1,47 +1,63 @@
 import cv2
 import numpy as np
-import picamera2
+# from picamera2 import Picamera2
+# from libcamera import controls
 import time
 
 class Vision:
     def __init__(self) :
-        #Set camera Properties
-        self.cap = picamera2.Picamera2()  
-        self.config = self.cap.create_video_configuration(main={"format":'XRGB8888',"size":(1640,1232)})
-        self.cap.configure(self.config)
-        self.cap.start()
+        # #Set camera Properties
+        # self.cap = Picamera2()  
+        # self.config = self.cap.create_video_configuration(main={"format":'XRGB8888',"size":(1640,1232)})
+        # self.cap.configure(self.config)
+        # self.cap.align_configuration(self.config)
+        # self.cap.configure(self.config)
+        # self.cap.set_controls({"AwbEnable": False, "ExposureTime": 1000, "AnalogueGain": 2.0})
+        # self.cap.start()
 
         self.cap = cv2.VideoCapture(0)
         # self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 308)
         # self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 410)
         # Constants
-        self.MIN_CONTOUR_AREA_THRESHOLD = 5000
+        self.MIN_CONTOUR_AREA_THRESHOLD = 500
         # Libs
         self.detected_objects_count = {}
         self.masks = {}
         self.center_points_dict = {}
 
+
+    #Note need to be chnaged
     color_ranges = {
+        'wall': (np.array([39, 0, 0]), np.array([162, 255, 255])),
         'yellow': (np.array([9, 85, 0]), np.array([19  , 255, 255])),
-        'blue': (np.array([39, 0, 0]), np.array([162, 255, 255])),
+        #'blue': (np.array([39, 0, 0]), np.array([162, 255, 255])),
         'green': (np.array([33, 0, 0]), np.array([94, 255, 255])),
         'orange': (np.array([0, 0, 157]), np.array([11, 255, 255])),
-        'black': (np.array([0, 0, 43]), np.array([179, 55, 109])),
-        'wall': (np.array([14, 24, 0]), np.array([32, 192, 255]))
+        'black': (np.array([0, 0, 43]), np.array([179, 55, 109]))
+        
     }
 
     contour_colors = {
+        'wall': (128, 0, 128),
         'yellow': (255, 255, 0),
-        'blue': (255, 0, 0),
+        #'blue': (255, 0, 0),
         'green': (0, 128, 0),
         'orange': (255, 200, 0),
-        'black': (0, 0, 0),
-        'wall': (128, 0, 128)
-    }
+        'black': (0, 0, 0)
         
+    }
+
+
+
+
+    
+
     def detect_color_objects(self, frame, color_range, contour_color, color_name):
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv_frame, color_range[0], color_range[1])
+
+        # Create a mask to exclude the top 100 pixels
+        mask[:100, :] = 0
 
         # Preprocess the mask
         mask = cv2.GaussianBlur(mask, (5, 5), 0)
@@ -52,15 +68,38 @@ class Vision:
         # Find contours in the mask
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        # Draw a line to indicate the border
+        cv2.line(frame, (0, 100), (frame.shape[1], 100), (0, 0, 255), 2)
+
         # Draw contour outlines and calculate center points
         detected_objects = 0
         center_points = []
         estimated_distances = []
+        # Initialize variables for average top position calculation
+        top_positions = []
 
         for contour in contours:
             if cv2.contourArea(contour) > self.MIN_CONTOUR_AREA_THRESHOLD:
                 cv2.drawContours(frame, [contour], -1, contour_color, 2)
                 detected_objects += 1
+
+                if color_name == 'wall':
+                    # Get the topmost point of the contour
+                    top_point = tuple(contour[contour[:, :, 1].argmin()][0])
+                    # Check if the top point falls within the specified x-range
+                    if (frame.shape[1] // 3) <= top_point[0] <= ((frame.shape[1] // 3) * 2):
+                        if top_point[1] < (frame.shape[0] // 2):
+                            top_positions.append(top_point[1])  # Y-coordinate
+
+                            # Draw a circle at the top point
+                            cv2.circle(frame, top_point, 5, contour_color, -1)
+
+                            # Draw a line across the top point, always across the x-axis
+                            cv2.line(frame, (0, top_point[1]), (frame.shape[1], top_point[1]), contour_color, 1)
+
+                            # Show Y-coordinate numbers on the Y-axis
+                            cv2.putText(frame, str(top_point[1]), (10, top_point[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, contour_color, 2)
+
 
                 M = cv2.moments(contour)
                 if M["m00"] != 0:
@@ -89,7 +128,7 @@ class Vision:
     def visualize(self, frame, masks):
         cv2.imshow('Object Detection', frame)
         cv2.imshow('Yellow Mask', masks['yellow'])
-        cv2.imshow('Blue Mask', masks['blue'])
+        #cv2.imshow('Blue Mask', masks['blue'])
         cv2.imshow('Green Mask', masks['green'])
         cv2.imshow('Orange Mask', masks['orange'])
         cv2.imshow('Black Mask', masks['black'])
