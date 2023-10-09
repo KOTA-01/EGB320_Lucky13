fbbc5258fe9c10d46af2547d953ab9d2d2950a5d
KOTA-01 <j.bird02@outlook.com>
2023-10-10 02:24:20 +1000

    Update collect.py

--- item_collection/collect.py
+++ item_collection/collect.py
@@ -2,49 +2,53 @@
 
 # Libs
 from gpiozero import AngularServo
-from time import *
+from time import sleep
 import RPi.GPIO as GPIO
 
 class Collection:
     def __init__(self):
         # Pin SetUp
+        GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
         GPIO.setup(23, GPIO.OUT)
         GPIO.setup(24, GPIO.OUT)
+        GPIO.setup(27, GPIO.IN)  # Define GPIO pin 27 as an input
+        GPIO.setup(17, GPIO.IN)  # Define GPIO pin 17 as an input
+        GPIO.setup(17, GPIO.IN)  # Define GPIO pin 17 as an input
 
         # Set PWM frequencies as 50Hz
         self.pVert = GPIO.PWM(23, 50)
         self.pHori = GPIO.PWM(24, 50)
 
     def Upack(self):
-        self.Servo_Vert(self,"high")
-        #self.Servo_Horz(self,"wide")
-        time.sleep(0.5)
+        self.Servo_Vert("high")
+        # self.Servo_Horz("wide")
+        sleep(0.5)
         return True
         
     def Drift_mode(self):
-        self.Servo_Vert(self,"high")
-        time.sleep(0.5)
+        self.Servo_Vert("high")
+        sleep(0.5)
         return True
 
     def Deliver(self):
         self.Servo_Vert("low")
-        time.sleep(0.5)
+        sleep(0.5)
         self.Servo_Horz("wide")
         return True
     
     def Servo_Vert(self, level):
-        if level == "high" & (not GPIO.input(27)):
+        if level == "high" and not GPIO.input(27):
             self.pVert.start(5)
             while not GPIO.input(27):
                 self.pVert.ChangeDutyCycle(6.5) 
                 if GPIO.input(27): 
                     self.pVert.ChangeDutyCycle(7) # Hold stop position
                     return True 
-        elif level == "low" & (not GPIO.input (17)):
+        elif level == "low" and not GPIO.input(17):
             self.pVert.start(5)
             while not GPIO.input(17):
                 self.pVert.ChangeDutyCycle(7.5)
-                if GPIO.input(27):
+                if GPIO.input(17):
                     self.pVert.ChangeDutyCycle(7) # Hold stop position
                     return True
         else:
@@ -52,29 +56,29 @@ class Collection:
             return True
         
     def Servo_Horz(self, spread):
-        if spread == "wide" & (not GPIO.input(27)):
+        if spread == "wide" and not GPIO.input(27):
             self.pHori.start(5)
             while not GPIO.input(27):
-                self.pVert.ChangeDutyCycle(6.5) 
+                self.pHori.ChangeDutyCycle(6.5) 
                 if GPIO.input(27): 
-                    self.pVert.ChangeDutyCycle(7) # Hold stop position
+                    self.pHori.ChangeDutyCycle(7) # Hold stop position
                     return True 
-        elif spread == "narrow" & (not GPIO.input (17)):
-            self.pVert.start(5)
+        elif spread == "narrow" and not GPIO.input(17):
+            self.pHori.start(5)
             while not GPIO.input(17):
-                self.pVert.ChangeDutyCycle(7.5)
-                if GPIO.input(27):
-                    self.pVert.ChangeDutyCycle(7) # Hold stop position
+                self.pHori.ChangeDutyCycle(7.5)
+                if GPIO.input(17):
+                    self.pHori.ChangeDutyCycle(7) # Hold stop position
                     return True
-        elif spread == "close" & (not GPIO.input (17)):
-            self.pVert.start(5)
+        elif spread == "close" and not GPIO.input(17):
+            self.pHori.start(5)
             while not GPIO.input(17):
-                self.pVert.ChangeDutyCycle(7.5)
-                if GPIO.input(27):
-                    self.pVert.ChangeDutyCycle(7) # Hold stop position
+                self.pHori.ChangeDutyCycle(7.5)
+                if GPIO.input(17):
+                    self.pHori.ChangeDutyCycle(7) # Hold stop position
                     return True
         else:
-            self.pVert.ChangeDutyCycle(7) # Hold stop position
+            self.pHori.ChangeDutyCycle(7) # Hold stop position
             return True
         
     def Clean(self):
