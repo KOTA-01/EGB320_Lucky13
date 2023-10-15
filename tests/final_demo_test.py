03c7cb63251d0862bd58933cc165600ba5c60a86
OniQUT <145178398+OniQUT@users.noreply.github.com>
2023-10-15 21:35:23 +1000

    integration changes

--- tests/final_demo_test.py
+++ tests/final_demo_test.py
@@ -65,54 +65,61 @@ from mobility.motor_control import stop
 
 if __name__ == '__main__':
 
-	# Wrap everything in a try except case that catches KeyboardInterrupts. 
-	# In the exception catch code attempt to Stop the CoppeliaSim so don't have to Stop it manually when pressing CTRL+C
-	try:
+    # Wrap everything in a try except case that catches KeyboardInterrupts. 
+    # In the exception catch code attempt to Stop the CoppeliaSim so don't have to Stop it manually when pressing CTRL+C
+    try:
 
-		DFRobot_DC_Motor()
-		DFRobot_DC_Motor_IIC(DFRobot_DC_Motor)
-		order_reader = OrderReader()
-		bot = robot()
-		# State machine initialization
-		order = 2
-		drive = 3
-		aisleNav = 4
-		exit_aisle = 5
-		drop_off = 6
-		done = 7
+        DFRobot_DC_Motor()
+        DFRobot_DC_Motor_IIC(DFRobot_DC_Motor)
+        order_reader = OrderReader()
+        bot = robot()
+        # State machine initialization
+        order = 2
+        drive = 3
+        aisleNav = 4
+        exit_aisle = 5
+        drop_off = 6
+        done = 7
 
-		state = order
-		
-		while True:
-			if state == order:
-				order_data = order_reader.ReadOrder("Order_1.csv")
-				print(order_data)
-				print("RED LED - I am searching for the item!")
-				state = aisleNav
-			
-			elif state == drive:
-				bot.nav_to_aisle()
-				state = aisleNav
+        state = order
+        completed_orders = 0
+        
+        while True:
+            if state == order:
+                order_data = order_reader.ReadOrder("Order_1.csv")
+                print(order_data)
+                print("RED LED - I am searching for the item!")
+                state = aisleNav
+            
+            elif state == drive:
+                bot.nav_to_aisle()
+                state = aisleNav
 
-			elif state == aisleNav:
-				bot.run()
-				state = exit_aisle
+            elif state == aisleNav:
+                bot.run()
+                state = exit_aisle
 
-			elif state == exit_aisle:
-				print("Exiting aisle")
-				bot.exiting()
-				state = done
+            elif state == exit_aisle:
+                print("Exiting aisle")
+                bot.exiting()
+                state = done
 
-			elif state == drop_off:
-				print("Searching for Packing bay")
-				bot.navigate_to_packing_bay()
-				state = done
+            elif state == drop_off:
+                print("Searching for Packing bay")
+                bot.navigate_to_packing_bay()
+                state = done
 
-			elif state == done:
-				print("Completed :3")
-				stop()
-				break
+            elif state == done:
+                print("Completed :3")
+                stop()
+                completed_orders += 1
 
-	except KeyboardInterrupt as e:
-		stop()
+                if completed_orders >= 3:
+                    print(f"{completed_orders} orders processed, stopping.")
+                    break
+
+                state = order
+
+    except KeyboardInterrupt as e:
+        stop()
 
