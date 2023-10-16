90495532848946f511616159f0cef071c9958916
OniQUT <145178398+OniQUT@users.noreply.github.com>
2023-10-17 09:39:47 +1000

    indentation

--- tests/final_demo_test.py
+++ tests/final_demo_test.py
@@ -8,52 +8,52 @@ import time
 from DFRobot_RaspberryPi_DC_Motor import THIS_BOARD_TYPE, DFRobot_DC_Motor_IIC as Board
 
 if THIS_BOARD_TYPE:
-  board = Board(1, 0x10)    # RaspberryPi select bus 1, set address to 0x10
+	board = Board(1, 0x10)    # RaspberryPi select bus 1, set address to 0x10
 else:
-  board = Board(7, 0x10)    # RockPi select bus 7, set address to 0x10
+	board = Board(7, 0x10)    # RockPi select bus 7, set address to 0x10
 
 def board_detect():
-  l = board.detecte()
-  print("Board list conform:")
-  print(l)
+	l = board.detecte()
+	print("Board list conform:")
+	print(l)
 
 ''' print last operate status, users can use this variable to determine the result of a function call. '''
 def print_board_status():
-  if board.last_operate_status == board.STA_OK:
-    print("board status: everything ok")
-  elif board.last_operate_status == board.STA_ERR:
-    print("board status: unexpected error")
-  elif board.last_operate_status == board.STA_ERR_DEVICE_NOT_DETECTED:
-    print("board status: device not detected")
-  elif board.last_operate_status == board.STA_ERR_PARAMETER:
-    print("board status: parameter error, last operate no effective")
-  elif board.last_operate_status == board.STA_ERR_SOFT_VERSION:
-    print("board status: unsupport board framware version")
+	if board.last_operate_status == board.STA_OK:
+		print("board status: everything ok")
+	elif board.last_operate_status == board.STA_ERR:
+		print("board status: unexpected error")
+	elif board.last_operate_status == board.STA_ERR_DEVICE_NOT_DETECTED:
+		print("board status: device not detected")
+	elif board.last_operate_status == board.STA_ERR_PARAMETER:
+		print("board status: parameter error, last operate no effective")
+	elif board.last_operate_status == board.STA_ERR_SOFT_VERSION:
+		print("board status: unsupport board framware version")
 
 if __name__ == "__main__":
 
-  board_detect()    # If you forget address you had set, use this to detected them, must have class instance
+	board_detect()    # If you forget address you had set, use this to detected them, must have class instance
 
-  # Set board controler address, use it carefully, reboot module to make it effective
-  '''
-  board.set_addr(0x10)
-  if board.last_operate_status != board.STA_OK:
-    print("set board address faild")
-  else:
-    print("set board address success")
-  '''
+	# Set board controler address, use it carefully, reboot module to make it effective
+	'''
+	board.set_addr(0x10)
+	if board.last_operate_status != board.STA_OK:
+		print("set board address faild")
+	else:
+		print("set board address success")
+	'''
 
-  while board.begin() != board.STA_OK:    # Board begin and check board status
-    print_board_status()
-    print("board begin faild")
-    time.sleep(2)
-  print("board begin success")
+	while board.begin() != board.STA_OK:    # Board begin and check board status
+		print_board_status()
+		print("board begin faild")
+		time.sleep(2)
+	print("board begin success")
 
-  board.set_encoder_enable(board.ALL)                 # Set selected DC motor encoder enable
-  # board.set_encoder_disable(board.ALL)              # Set selected DC motor encoder disable
-  board.set_encoder_reduction_ratio(board.ALL, 43)    # Set selected DC motor encoder reduction ratio, test motor reduction ratio is 43.8
+	board.set_encoder_enable(board.ALL)                 # Set selected DC motor encoder enable
+	# board.set_encoder_disable(board.ALL)              # Set selected DC motor encoder disable
+	board.set_encoder_reduction_ratio(board.ALL, 43)    # Set selected DC motor encoder reduction ratio, test motor reduction ratio is 43.8
 
-  board.set_moter_pwm_frequency(1000)   # Set DC motor pwm frequency to 1000HZ
+	board.set_moter_pwm_frequency(1000)   # Set DC motor pwm frequency to 1000HZ
 
 import time
 from navigation.itemIndex import item_to_index
@@ -65,61 +65,61 @@ from mobility.motor_control import stop
 
 if __name__ == '__main__':
 
-    # Wrap everything in a try except case that catches KeyboardInterrupts. 
-    # In the exception catch code attempt to Stop the CoppeliaSim so don't have to Stop it manually when pressing CTRL+C
-    try:
+		# Wrap everything in a try except case that catches KeyboardInterrupts. 
+		# In the exception catch code attempt to Stop the CoppeliaSim so don't have to Stop it manually when pressing CTRL+C
+		try:
 
-        DFRobot_DC_Motor()
-        DFRobot_DC_Motor_IIC(DFRobot_DC_Motor)
-        order_reader = OrderReader()
-        bot = robot()
-        # State machine initialization
-        order = 2
-        drive = 3
-        aisleNav = 4
-        exit_aisle = 5
-        drop_off = 6
-        done = 7
+				DFRobot_DC_Motor()
+				DFRobot_DC_Motor_IIC(DFRobot_DC_Motor)
+				order_reader = OrderReader()
+				bot = robot()
+				# State machine initialization
+				order = 2
+				drive = 3
+				aisleNav = 4
+				exit_aisle = 5
+				drop_off = 6
+				done = 7
 
-        state = order
-        completed_orders = 0
-        
-        while True:
-            if state == order:
-                order_data = order_reader.ReadOrder("Order_1.csv")
-                print(order_data)
-                print("RED LED - I am searching for the item!")
-                state = aisleNav
-            
-            elif state == drive:
-                bot.nav_to_aisle()
-                state = aisleNav
+				state = order
+				completed_orders = 0
+				
+				while True:
+						if state == order:
+								order_data = order_reader.ReadOrder("Order_1.csv")
+								print(order_data)
+								print("RED LED - I am searching for the item!")
+								state = aisleNav
+						
+						elif state == drive:
+								bot.nav_to_aisle()
+								state = aisleNav
 
-            elif state == aisleNav:
-                bot.run()
-                state = exit_aisle
+						elif state == aisleNav:
+								bot.run()
+								state = exit_aisle
 
-            elif state == exit_aisle:
-                print("Exiting aisle")
-                bot.exiting()
-                state = done
+						elif state == exit_aisle:
+								print("Exiting aisle")
+								bot.exiting()
+								state = done
 
-            elif state == drop_off:
-                print("Searching for Packing bay")
-                bot.navigate_to_packing_bay()
-                state = done
+						elif state == drop_off:
+								print("Searching for Packing bay")
+								bot.navigate_to_packing_bay()
+								state = done
 
-            elif state == done:
-                print("Completed :3")
-                stop()
-                completed_orders += 1
+						elif state == done:
+								print("Completed :3")
+								stop()
+								completed_orders += 1
 
-                if completed_orders >= 3:
-                    print(f"{completed_orders} orders processed, stopping.")
-                    break
+								if completed_orders >= 3:
+										print(f"{completed_orders} orders processed, stopping.")
+										break
 
-                state = order
+								state = order
 
-    except KeyboardInterrupt as e:
-        stop()
+		except KeyboardInterrupt as e:
+				stop()
 
